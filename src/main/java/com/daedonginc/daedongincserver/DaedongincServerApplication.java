021cc39a076cd16b9c9112293c6a663a5f3a93fd
kdomo <dongho5088@naver.com>
2023-03-19 22:23:26 +0900

    init: project initial

---
+++ src/main/java/com/daedonginc/daedongincserver/DaedongincServerApplication.java
@@ -0,0 +1,13 @@
+package com.daedonginc.daedongincserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaedongincServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaedongincServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daedonginc/daedongincserver/DaedongincServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daedonginc.daedongincserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaedongincServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
