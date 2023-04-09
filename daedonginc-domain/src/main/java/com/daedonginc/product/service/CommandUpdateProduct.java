fe60a17d29c054c516f76e2dc3c1534413db1d97
kdomo <dongho5088@naver.com>
2023-04-09 19:15:41 +0900

    [#35] feat: 상품 기능 구현 (상품 수정, 상품 삭제) :: Product

---
+++ daedonginc-domain/src/main/java/com/daedonginc/product/service/CommandUpdateProduct.java
@@ -0,0 +1,52 @@
+package com.daedonginc.product.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.daedonginc.entity.category.CategoryEntity;
+import com.daedonginc.model.category.CategoryLevel;
+import com.daedonginc.product.exception.CategoryLevelIsNotSuitableException;
+import com.daedonginc.product.usecase.CommandUpdateProductUseCase;
+import com.daedonginc.service.category.CategoryQuery;
+import com.daedonginc.service.product.ProductCommand;
+import com.daedonginc.service.product.ProductQuery;
+
+/**
+ * @author domo
+ * Created on 2023/04/05
+ */
+@Service
+@Transactional
+public class CommandUpdateProduct implements CommandUpdateProductUseCase {
+	private final ProductCommand productCommand;
+	private final ProductQuery productQuery;
+	private final CategoryQuery categoryQuery;
+
+	public CommandUpdateProduct(
+			final ProductCommand productCommand,
+			final ProductQuery productQuery,
+			final CategoryQuery categoryQuery
+	) {
+		this.productCommand = productCommand;
+		this.productQuery = productQuery;
+		this.categoryQuery = categoryQuery;
+	}
+
+	@Override
+	public void command(final Command command) {
+		CategoryEntity categoryEntity = categoryQuery.findById(command.categoryId());
+
+		if (categoryEntity.getCategoryLevel() != CategoryLevel.MIDDLE) {
+			throw new CategoryLevelIsNotSuitableException("하위 카테고리만 등록 가능합니다.");
+		}
+		
+		productQuery.findById(command.productId()).update(
+				categoryEntity,
+				command.name(),
+				command.volume(),
+				command.size(),
+				command.partMaterial(),
+				command.description(),
+				command.imageUrl());
+	}
+}
--- daedonginc-interface/src/main/java/com/daedonginc/api/product/ProductController.java
+++ daedonginc-interface/src/main/java/com/daedonginc/api/product/ProductController.java
@@ -3,18 +3,23 @@ package com.daedonginc.api.product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daedonginc.api.product.dto.CreateProductRequestDto;
 import com.daedonginc.api.product.dto.ProductResponseDto;
+import com.daedonginc.api.product.dto.UpdateProductRequestDto;
 import com.daedonginc.api.product.mapper.ProductMapper;
 import com.daedonginc.product.domain.Product;
 import com.daedonginc.product.usecase.CommandCreateProductUseCase;
+import com.daedonginc.product.usecase.CommandDeleteProductUseCase;
+import com.daedonginc.product.usecase.CommandUpdateProductUseCase;
 import com.daedonginc.product.usecase.QueryProductAllUseCase;
 import com.daedonginc.product.usecase.QueryProductByIdUseCase;
 
@@ -28,15 +33,21 @@ public class ProductController {
 	private final QueryProductAllUseCase queryProductAllUseCase;
 	private final QueryProductByIdUseCase queryProductByIdUseCase;
 	private final CommandCreateProductUseCase commandCreateProductUseCase;
+	private final CommandUpdateProductUseCase commandUpdateProductUseCase;
+	private final CommandDeleteProductUseCase commandDeleteProductUseCase;
 
 	public ProductController(
 			final QueryProductAllUseCase queryProductAllUseCase,
 			final QueryProductByIdUseCase queryProductByIdUseCase,
-			final CommandCreateProductUseCase commandCreateProductUseCase
+			final CommandCreateProductUseCase commandCreateProductUseCase,
+			final CommandUpdateProductUseCase commandUpdateProductUseCase,
+			final CommandDeleteProductUseCase commandDeleteProductUseCase
 	) {
 		this.queryProductAllUseCase = queryProductAllUseCase;
 		this.queryProductByIdUseCase = queryProductByIdUseCase;
 		this.commandCreateProductUseCase = commandCreateProductUseCase;
+		this.commandUpdateProductUseCase = commandUpdateProductUseCase;
+		this.commandDeleteProductUseCase = commandDeleteProductUseCase;
 	}
 
 	@GetMapping
@@ -58,7 +69,8 @@ public class ProductController {
 	}
 
 	@PostMapping
-	public ProductResponseDto createProduct(@RequestBody @Validated CreateProductRequestDto dto) {
+	public ProductResponseDto createProduct(
+			@RequestBody @Validated final CreateProductRequestDto dto) {
 		Product product = commandCreateProductUseCase.command(
 				new CommandCreateProductUseCase.Command(
 						dto.categoryId(),
@@ -72,5 +84,33 @@ public class ProductController {
 		);
 
 		return ProductMapper.toResponseDto(product);
+	}
+
+	@PutMapping("/{id}")
+	public void updateProduct(
+			@PathVariable final Long id,
+			@RequestBody @Validated final UpdateProductRequestDto dto
+	) {
+		commandUpdateProductUseCase.command(
+				new CommandUpdateProductUseCase.Command(
+						id,
+						dto.categoryId(),
+						dto.name(),
+						dto.volume(),
+						dto.size(),
+						dto.partMaterial(),
+						dto.description(),
+						dto.imageUrl()
+				)
+		);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteProduct(
+			@PathVariable final Long id
+	) {
+		commandDeleteProductUseCase.command(
+				new CommandDeleteProductUseCase.Command(id)
+		);
 	}
 }
