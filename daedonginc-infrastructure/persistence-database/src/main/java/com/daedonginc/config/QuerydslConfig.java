92898bcc9b6a49715298b19739a8f8312bd05be2
kdomo <dongho5088@naver.com>
2023-04-30 16:20:07 +0900

    [#69] chore: QueryDSL 설정

---
+++ daedonginc-infrastructure/persistence-database/src/main/java/com/daedonginc/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package com.daedonginc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+/**
+ * @author domo
+ * Created on 2023/04/30
+ */
+@Configuration
+public class QuerydslConfig {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(entityManager);
+	}
+}
